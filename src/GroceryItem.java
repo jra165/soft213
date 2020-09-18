8d1933000b5ad2fa4339ee02176f184c2ea822b8
kyle-t-lee <36808274+kyle-t-lee@users.noreply.github.com>
2020-09-18 00:53:09 -0400

    Setup Classes
    Created project and class files

---
+++ src/GroceryItem.java
@@ -0,0 +1,23 @@
+public class GroceryItem {
+	private String name;
+	private double price;
+	private boolean taxable;
+	
+	//public boolean equals(Object obj){ }
+	//public String toString() { }
+	public GroceryItem(char action, String name, double price, boolean taxable) {
+		this.name = name;
+		this.price = price;
+		this.taxable = taxable;
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+	}
+
+}
---
+++ src/ShoppingBag.java
@@ -0,0 +1,18 @@
+
+public class ShoppingBag {
+	private GroceryItem[] bag; // array-based implementation of the bag
+	private int size; // number of items currently in the bag
+	public ShoppingBag() { }
+	//private int find(GroceryItem item) { } // helper method to find an item
+	//private void grow() { } // helper method to grow the capacity
+	//public void add(GroceryItem item) { }
+	//public boolean remove(GroceryItem item) { }
+	//public double salesPrice() { } // sales total; the sum of the prices in the bag
+	//public double salesTax() { } // sales tax total of the taxable items in the bag
+	//public void print() { }
+	//public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
