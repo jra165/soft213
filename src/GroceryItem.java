edef9d8ba88484998722187b640fdfcf680a1d1c
josh <jra165@scarletmail.rutgers.edu>
2020-09-23 15:16:12 -0400

    documented GroceryItem class
    left comments with proper documentation guidelines

--- src/GroceryItem.java
+++ src/GroceryItem.java
@@ -1,28 +1,59 @@
 import java.text.DecimalFormat;
 
+/**
+The GroceryItem class represents the properties and methods associated with the GroceryItem object.
+Properties include name, price, and whether or not GroceryItem is taxable.
+Methods include getName, getPrice, getTaxable, equals, and toString (described above each respective method)
+@author Joshua Atienza, Kyle Lee
+*/
+
 public class GroceryItem {
 	private String name;
 	private double price;
 	private boolean taxable;
 	
+	/**
+	Creates a GroceryItem with the specified name, price, and taxability.
+	@param name The name of the GroceryItem
+	@param price The price of the GroceryItem
+	@param taxable Whether or not the GroceryItem is taxable
+	*/
 	public GroceryItem(String name, double price, boolean taxable) {
 		this.name = name;
 		this.price = price;
 		this.taxable = taxable;
 	}
 	
+	/**
+	Gets the name of GroceryItem
+	@return name of GroceryItem
+	*/
 	public String getName() {
 		return name;
 	}
-	
+
+	/**
+	Gets the price of GroceryItem
+	@return price of GroceryItem
+	*/
 	public double getPrice() {
 		return price;
 	}
 	
+	/**
+	Gets the taxability of GroceryItem
+	@return true if GroceryItem is taxable, false otherwise
+	*/
 	public boolean getTaxable() {
 		return taxable;
 	}
-	
+
+	/**
+	Checks if GroceryItem is equivalent to obj being compared to
+	Checks if obj is instanceof GroceryItem and if properties between the two objects are equivalent
+	@param obj The object being compared to a particular GroceryItem
+	@return true if GroceryItem is equivalent to object, false otherwise
+	*/
 	// double check usage of .equals() and .compare()
 	public boolean equals(Object obj){
 		if (obj == this) {
@@ -37,7 +68,11 @@ public class GroceryItem {
 		return false;
 	}
 	
-	
+	/**
+	Converts GroceryItem object to String representation.
+	String representation follows the format "itemName: $xx.xx : tax free."
+	@return groceryItemString The string representation of GroceryItem obejct
+	*/
 	public String toString() {
 		
 		String tax_check;
@@ -51,9 +86,11 @@ public class GroceryItem {
 			tax_check = "is taxable";
 		}
 		
-		return this.name + ": $" + rounded_price + " : " + tax_check;
+		String groceryitemString = this.name + ": $" + rounded_price + " : " + tax_check;
+		return groceryitemString;
 		
 	}
+	
 	
 	public static void main(String[] args) {
 		
--- src/ShoppingBag.java
+++ src/ShoppingBag.java
@@ -149,6 +149,8 @@ public class ShoppingBag {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
 
 	}
 
